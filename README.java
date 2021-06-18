5c0e0464b352f93c6fc832eae9cf7b5afaa40ffa
Mohammed Olaiya <mohammedolaiya@gmail.com>
2021-06-17 21:02:48 -0400

    Update README.java

--- README.java
+++ README.java
@@ -56,4 +56,16 @@ class Mayan extends Language {
     System.out.println("The language follows the word order: " + this.wordOrder);
     System.out.println("Fun fact: " + this.name + " is an ergative language.");
     };
-}
+};
+
+class SineTibetan extends Language {
+
+  SinoTibetan(String languageName, int speakers) {
+    super(languageName, speakers, "Asia", "subject-object-verb");
+  };
+    if (languageName.contains("Chinese")) {
+      this.wordOrder = "subject-verb-object";
+    };
+};
+
+
